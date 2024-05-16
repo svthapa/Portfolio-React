a6d271af0bb7f528ccf2ed8624b9bcda4fe307e6
svthapa <60999773+svthapa@users.noreply.github.com>
2024-05-15 19:20:58 -0500

    update resume

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="/resume.docx"
+                  href="/Resume.docx"
                   download
                 >
                   Resume
